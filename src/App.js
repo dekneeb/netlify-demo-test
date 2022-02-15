74104eabdabdd0337a896234f9d5b663de870b7d
Denee Boyd <deneeoliviaboyd@gmail.com>
2022-02-15 10:06:29 -0600

    reconfigured minions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  const [gifs, setGifs] = useState([]);
+	useEffect(() => {
+		function getGifData() {
+			const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_GIPHY_KEY}&q=minions&limit=10&rating=G&lang=en`;
+			fetch(url)
+				.then((res) => res.json())
+				.then((res) => {
+					console.log('We have data!', res.data);
+					setGifs(res.data);
+				})
+				.catch(console.error);
+		}
+		getGifData();
+	}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <h1>Testing Netlify Deployment</h1>
+   {gifs.map(gif=>
+     <img key={gif.id} src={gif.images.downsized_medium.url} />
+   )}
     </div>
   );
 }
